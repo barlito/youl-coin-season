c3360223b83708762aa30bd780e3ffbf97a80209
Axel Barlet <axelbarlet16@gmail.com>
2023-09-12 23:20:29 +0200

    feat: Remove make filess + add Make submmodule

--- make/app/package.mk
+++
@@ -1,5 +0,0 @@
-### Package rules
-
-package.barlito_utils.install:
-	docker exec -t $(app_container_id) bash -c "composer config repositories.barlito/utils vcs https://github.com/barlito/utils"
-	docker exec -t $(app_container_id) bash -c "composer require barlito/utils"
--- make/stack/_entrypoint.mk
+++
@@ -1,5 +0,0 @@
-# Dev rules
-include make/stack/development.mk
-
-# Docker rules
-include make/stack/docker.mk
