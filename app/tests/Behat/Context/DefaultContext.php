bb3265f0784dd5a7c553a9c1fd2266ce1354f1f5
Axel Barlet <axelbarlet16@gmail.com>
2023-09-14 22:51:49 +0200

    feat: Setup phpunit and behat + add no tests, tests are too annoying too add here

---
+++ app/tests/Behat/Context/DefaultContext.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Behat\Context;
+
+use Behat\Behat\Context\Context;
+
+/**
+ * This context class contains the definitions of the steps used by the demo
+ * feature file. Learn how to get started with Behat and BDD on Behat's website.
+ *
+ * @see http://behat.org/en/latest/quick_start.html
+ */
+final class DefaultContext implements Context
+{
+    /**
+     * Ensure to fully reset the test database fixtures features, allowing easy knowledge of the current
+     * database state at the beginning of each features
+     *
+     * @Given I reload the fixtures
+     *
+     * @BeforeFeature
+     */
+    public static function prepareFixtures(): void
+    {
+//        system('bin/console hautelook:fixtures:load -n --env="test"');
+    }
+
+    /**
+     * Clean cache before feature
+     *
+     * @BeforeFeature @needCleanCachePool
+     */
+    public static function clearCachePool(): void
+    {
+        system('bin/console cache:pool:clear cache.app --env="test"');
+    }
+}
